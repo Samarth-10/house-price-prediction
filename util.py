abb687e230d1c5085b52e95df5752764cde388b6
Samarth-10 <101192291+Samarth-10@users.noreply.github.com>
2022-09-21 16:39:18 +0530

    Update util.py

--- util.py
+++ util.py
@@ -1,45 +1,98 @@
+import joblib
 import json
-import pickle
-from webbrowser import get
 import numpy as np
-__locations=None
-__data_columns=None
-__model=None
+import base64
+import cv2
+from wavelet import w2d
 
-def get_location_names():
-    __data_columns=json.load(open("columns.json",'r'))['data_columns']
-    __locations=__data_columns[3:]
-    return __locations
+__class_name_to_number = {}
+__class_number_to_name = {}
 
-def get_estimated_price(location,sqft,bhk,bath):
-    __model=pickle.load(open("banglore_home_prices_model.pickle",'rb'))
-    try:
-        loc_index=__data_columns.index(location=lower())
-    except:
-        loc_index=-1
-    
-    x = np.zeros(len(__data_columns))
-    x[0] = sqft
-    x[1] = bath
-    x[2] = bhk
-    if loc_index >= 0:
-        x[loc_index] = 1
-    return round(__model.predict([x])[0],2)
+__model = None
+
+def classify_image(image_base64_data, file_path=None):
+
+    imgs = get_cropped_image_if_2_eyes(file_path, image_base64_data)
+
+    result = []
+    for img in imgs:
+        scalled_raw_img = cv2.resize(img, (32, 32))
+        img_har = w2d(img, 'db1', 5)
+        scalled_img_har = cv2.resize(img_har, (32, 32))
+        combined_img = np.vstack((scalled_raw_img.reshape(32 * 32 * 3, 1), scalled_img_har.reshape(32 * 32, 1)))
+
+        len_image_array = 32*32*3 + 32*32
+
+        final = combined_img.reshape(1,len_image_array).astype(float)
+        result.append({
+            'class': class_number_to_name(__model.predict(final)[0]),
+            'class_probability': np.around(__model.predict_proba(final)*100,2).tolist()[0],
+            'class_dictionary': __class_name_to_number
+        })
+
+    return result
+
+def class_number_to_name(class_num):
+    return __class_number_to_name[class_num]
 
 def load_saved_artifacts():
-    print("loading saved artifacts")
-    global __locations
-    global __data_columns
+    print("loading saved artifacts...start")
+    global __class_name_to_number
+    global __class_number_to_name
 
-    __data_columns=json.load(open("columns.json",'r'))['data_columns']
-    __locations=__data_columns[3:]
+    with open("/artifacts/class_dictionary.json", "r") as f:
+        __class_name_to_number = json.load(f)
+        __class_number_to_name = {v:k for k,v in __class_name_to_number.items()}
 
     global __model
-    __model=pickle.load(open("banglore_home_prices_model.pickle",'rb'))
+    if __model is None:
+        with open('/artifacts/saved_model.pkl', 'rb') as f:
+            __model = joblib.load(f)
+    print("loading saved artifacts...done")
 
-    print("Done loading artifacts")
+
+def get_cv2_image_from_base64_string(b64str):
     
-if __name__=='__main__':
+    encoded_data = b64str.split(',')[1]
+    nparr = np.frombuffer(base64.b64decode(encoded_data), np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    return img
+
+def get_cropped_image_if_2_eyes(image_path, image_base64_data):
+    face_cascade = cv2.CascadeClassifier('/opencv/haarcascades/haarcascade_frontalface_default.xml')
+    eye_cascade = cv2.CascadeClassifier('/opencv/haarcascades/haarcascade_eye.xml')
+
+    if image_path:
+        img = cv2.imread(image_path)
+    else:
+        img = get_cv2_image_from_base64_string(image_base64_data)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    cropped_faces = []
+    for (x,y,w,h) in faces:
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = img[y:y+h, x:x+w]
+            eyes = eye_cascade.detectMultiScale(roi_gray)
+            if len(eyes) >= 2:
+                cropped_faces.append(roi_color)
+    return cropped_faces
+
+def get_b64_test_image_for_virat():
+    with open("b64.txt") as f:
+        return f.read()
+
+if __name__ == '__main__':
     load_saved_artifacts()
-    print(get_location_names())
-    print(get_estimated_price("1st Phase JP Nagar",1000,1,1))
+
+    print(classify_image(get_b64_test_image_for_virat(), None))
+
+    # print(classify_image(None, "./test_images/federer1.jpg"))
+    # print(classify_image(None, "./test_images/federer2.jpg"))
+    # print(classify_image(None, "./test_images/virat1.jpg"))
+    # print(classify_image(None, "./test_images/virat2.jpg"))
+    # print(classify_image(None, "./test_images/virat3.jpg")) # Inconsistent result could be due to https://github.com/scikit-learn/scikit-learn/issues/13211
+    # print(classify_image(None, "./test_images/serena1.jpg"))
+    # print(classify_image(None, "./test_images/serena2.jpg"))
+    # print(classify_image(None, "./test_images/sharapova1.jpg"))
